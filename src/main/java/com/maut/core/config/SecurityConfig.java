e5f1df49dc588a7c8a6c16d29feb951b73ceeda8
Sam Schlachter <s.schlachter@unlimit.com>
2025-05-09 10:55:52 -0400

    more security context

--- src/main/java/com/maut/core/config/SecurityConfig.java
+++ src/main/java/com/maut/core/config/SecurityConfig.java
@@ -27,6 +27,10 @@ public class SecurityConfig {
                 .mvcMatchers(HttpMethod.POST, "/v1/admin/client-applications").permitAll()
                 // Permit unauthenticated GET requests to the status endpoint for health checks
                 .mvcMatchers(HttpMethod.GET, "/v1/status").permitAll()
+                // Permit unauthenticated GET requests to the hello endpoint
+                .mvcMatchers(HttpMethod.GET, "/v1/hello").permitAll()
+                // Permit unauthenticated POST requests to the session endpoint
+                .mvcMatchers(HttpMethod.POST, "/v1/session").permitAll()
                 // All other requests should be authenticated
                 .anyRequest().authenticated()
             );
